0339db1d79dffdb7bbf5140e2346fbca67568b26
Maximus R.D. Parmer <maxp@pdx.edu>
2014-12-19 15:42:37 -0800

    Integrates flake8 critiques.

--- SuspiciousLogger.py
+++ SuspiciousLogger.py
@@ -42,18 +42,20 @@ from pygeoip.const import ENCODING as GEOIP_ENC
 from threading import Thread, Lock, active_count as active_thread_count
 
 RFC3339_ZULU_FMT = '%Y-%m-%dT%H:%M:%S.%fZ'
+DIR_NAME = os.path.dirname(__file__)
 # CLIENT_SECRETS is name of a file containing the OAuth 2.0 information
 # <https://cloud.google.com/console#/project/803928506099/apiui>
-CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
-SESSION_STATE = os.path.join(os.path.dirname(__file__), 'SuspiciousLogger.dat')
-GEOIP_DATA = os.path.join(os.path.dirname(__file__), 'GeoIP_data', 'GeoLiteCity.dat')
+CLIENT_SECRETS = os.path.join(DIR_NAME, 'client_secrets.json')
+SESSION_STATE = os.path.join(DIR_NAME, 'SuspiciousLogger.dat')
+GEOIP_DATA = os.path.join(DIR_NAME, 'GeoIP_data', 'GeoLiteCity.dat')
 GEOIP = pygeoip.GeoIP(GEOIP_DATA, pygeoip.MMAP_CACHE)
+
 
 def oauthorize(client_secrets, session_file):
     """Takes a path to a client_secrets.json and a session.dat file,
     performs an OAuth auth-flow and returns a service object."""
-    # If the credentials don't exist or are invalid run through the native client
-    # flow. The Storage object will ensure that if successful the good
+    # If the credentials don't exist or are invalid run through the native
+    # client flow. The Storage object will ensure that if successful the good
     # credentials will get written back to the file.
     storage = oauth_file.Storage(session_file)
     credentials = storage.get()
@@ -62,18 +64,21 @@ def oauthorize(client_secrets, session_file):
         # Prevent obnoxious browser window launching if our session is expired
         faux_parser = argparse.ArgumentParser(parents=[tools.argparser])
         flow_flags = faux_parser.parse_args("--noauth_local_webserver".split())
-        scope = ['https://www.googleapis.com/auth/admin.reports.audit.readonly',
-                 'https://www.googleapis.com/auth/admin.reports.usage.readonly']
-        flow = client.flow_from_clientsecrets(client_secrets, scope=scope, \
-                              message=tools.message_if_missing(client_secrets))
+        scope = [
+            'https://www.googleapis.com/auth/admin.reports.audit.readonly',
+            'https://www.googleapis.com/auth/admin.reports.usage.readonly'
+        ]
+        err_msg = tools.message_if_missing(client_secrets)
+        flow = client.flow_from_clientsecrets(client_secrets, scope=scope,
+                                              message=err_msg)
         credentials = tools.run_flow(flow, storage, flow_flags)
 
-    # Create an httplib2.Http object to handle our HTTP requests and authorize it
-    # with our good Credentials.
+    # Create an httplib2.Http object to handle our HTTP requests and do auth
     http = httplib2.Http()
     http = credentials.authorize(http)
 
     return discovery.build('admin', 'reports_v1', http=http)
+
 
 def filter_collection(collection, collection_filter):
     """Tries to execute a query -- collection_filter -- against a collection
@@ -86,11 +91,13 @@ def filter_collection(collection, collection_filter):
         logging.critical("Authorization has expired, re-authing next run.")
         return {}
     except errors.HttpError, err:
-        logging.error("Collection of userKey=%s, actorIpAddress=%s, failed: %s",
-                      collection_filter['userKey'],
-                      collection_filter['actorIpAddress'],
-                      err)
+        logging.error(
+            "Collection of userKey=%s, actorIpAddress=%s, failed: %s",
+            collection_filter['userKey'],
+            collection_filter['actorIpAddress'],
+            err)
         return {}
+
 
 def fmt_response(response):
     """Provides basic formatting for some common collection.list fields."""
@@ -99,6 +106,7 @@ def fmt_response(response):
     if 'login_type' in response:
         log_fmt += response['login_type']
     return log_fmt.format(**response)
+
 
 def geoip_metro(ip_addr):
     """Transform an IP into a reasonably readable country and region code,
@@ -114,10 +122,12 @@ def geoip_metro(ip_addr):
     return u"{}, {}".format(unicode(metro, GEOIP_ENC),
                             unicode(country, GEOIP_ENC))
 
+
 def repack_collection(col):
     """Repacks a collection.list item into a dictionary with the record keyed
     off it's globally unique etag, ensuring merged collections are not
-    redundant while also flatting them quite a bit for greater ease in sorting."""
+    redundant while also flatting them quite a bit for greater ease in sorting
+    and formatting."""
     packed = {}
     for entry in col:
         etag = entry['etag']
@@ -126,7 +136,7 @@ def repack_collection(col):
                         u'ip':    entry['ipAddress'],
                         u'loc':   geoip_metro(entry['ipAddress']),
                         u'time':  datetime.strptime(entry['id']['time'],
-                                                    RFC3339_ZULU_FMT),}
+                                                    RFC3339_ZULU_FMT)}
         for event in entry['events']:
             if 'name' in event:
                 packed[etag]['event'] = event['name']
@@ -136,6 +146,7 @@ def repack_collection(col):
                         packed[etag][params['name']] = params['value']
         logging.debug("Repacked entry as: %s", packed[etag])
     return packed
+
 
 def valid_selector(selector):
     """Validator for the 'selector' arg type -- checks for one or more email
@@ -155,10 +166,14 @@ def valid_selector(selector):
             results.append(selector)
         else:
             try:
-                results.extend([ip for ip in IP(selector)])  # The IP is itself iterable
+                # iterate the IP iterable now, in case we're building a
+                # heterogenous list of selector types
+                results.extend([ip for ip in IP(selector)])
             except:
-                raise argparse.ArgumentTypeError("'{}' is not an IP or email.".format(selector))
+                raise argparse.ArgumentTypeError(
+                    "'{}' is not an IP or email.".format(selector))
     return results
+
 
 def set_collection_filter(collection_filter, selector):
     """Set up the collection filter based on the type of the selector."""
@@ -172,6 +187,7 @@ def set_collection_filter(collection_filter, selector):
         collection_filter['userKey'] = selector
 
     return collection_filter
+
 
 def query_worker(collection_filter, selector, responses):
     """A worker fn for authorizing then querying against the reports API.
@@ -191,20 +207,23 @@ def query_worker(collection_filter, selector, responses):
         with update_lock:
             responses.update(final_collection)
     else:
-        logging.info("Did not find results for %s, %s", selector, collection_filter)
+        logging.info("Did not find results for %s, %s",
+                     selector, collection_filter)
+
 
 def main(argv):
     """This tool composes the rudiments of exposing the various aspect of the
     admin-sdk reports API:
-        https://developers.google.com/admin-sdk/reports/v1/reference/activities/list"""
+    https://developers.google.com/admin-sdk/reports/v1/reference/activities/list
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-j', '--jobs', nargs=1, default=25, \
+    parser.add_argument('-j', '--jobs', nargs=1, default=25,
                         help="Maximum number of query threads to spawn.")
-    parser.add_argument('-v', '--verbose', dest='verbosity', \
-                        action='count', \
-                        default=0, \
+    parser.add_argument('-v', '--verbose', dest='verbosity',
+                        action='count',
+                        default=0,
                         help='One invocation: INFO level, two: DEBUG.')
-    parser.add_argument('selectors', type=valid_selector, \
+    parser.add_argument('selectors', type=valid_selector,
                         help='An IP, CIDR range, gmail address, comma separated\
                         list of all three or the word "all".')
 
@@ -212,7 +231,7 @@ def main(argv):
                                        eventname and additional filters.')
     subparsers.add_parser('list', help='List all events.')
 
-    event_parser = subparsers.add_parser('events', \
+    event_parser = subparsers.add_parser('events',
                                          help='Filter log lines by event type \
                                          and additional filters.')
     event_parser.add_argument('eventName')
@@ -226,7 +245,6 @@ def main(argv):
     logging.basicConfig(level=loglevel)
     logging.info("Log level set to: '%s'", logging.getLevelName(loglevel))
 
-
     # Set up the base collection filter
     collection_filter = {'applicationName': 'login'}
 
@@ -235,17 +253,20 @@ def main(argv):
         collection_filter['eventName'] = flags.eventName
         collection_filter['filters'] = flags.filters
 
-    responses = {} # this dict has new results .merge()'d within a lock by each thread
+    # responses dict has new results .merge()'d within a lock by each thread
+    responses = {}
     while len(flags.selectors) is not 0:
         if active_thread_count() < flags.jobs:
             selector = flags.selectors.pop()
-            worker = Thread(target=query_worker, args=(collection_filter, selector, responses))
+            worker = Thread(target=query_worker, args=(collection_filter,
+                                                       selector, responses))
             worker.start()
 
     while True:
         login_sequence = sorted(responses, key=lambda x: responses[x]['time'])
         if active_thread_count() is 1:
-            print u"\n".join([fmt_response(responses[k]) for k in login_sequence])
+            print u"\n".join([fmt_response(responses[k])
+                              for k in login_sequence])
             sys.exit(0)
 
 if __name__ == '__main__':
