3d0ac56037a2e0a57daa5e366b4ba8105c711262
Maximus R.D. Parmer <maxp@pdx.edu>
2014-12-19 16:43:56 -0800

    Space.

--- SuspiciousLogger.py
+++ SuspiciousLogger.py
@@ -101,7 +101,7 @@ def filter_collection(collection, collection_filter):
 
 def fmt_response(response):
     """Provides basic formatting for some common collection.list fields."""
-    log_fmt = u"{time} {ip: <40}{loc: <35}{actor: <18}{event} "
+    log_fmt = u"{time} {ip: <40}{loc: <35}{actor: <18} {event}"
     response['time'] = datetime.strftime(response['time'], RFC3339_ZULU_FMT)
     if 'login_type' in response:
         log_fmt += ' {login_type}'
